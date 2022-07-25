9acde77980e05ec097102bf9b589bd8f5b16be07
yplog <yalinpala@protonmail.com>
2022-07-25 11:55:44 +0300

    prisma module & service

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { PrismaService } from './utils/prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
