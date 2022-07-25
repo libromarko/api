9a0a795b969f4b5c0f9259d1fd11a90767d57a8e
yplog <yalinpala@protonmail.com>
2022-07-25 11:46:50 +0300

    prisma && swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './utils/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const prismaService = app.get(PrismaService);
+
+  const config = new DocumentBuilder()
+    .setTitle('libromarko')
+    .setDescription('libromarko API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await prismaService.enableShutdownHooks(app);
+  await app.listen(3001);
 }
+
 bootstrap();
